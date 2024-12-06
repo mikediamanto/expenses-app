4610b1f8f617e94f0538af573ad76df09e8c9990
Mike Diamantopoulos <mdiamantopoulos@novibet.com>
2024-12-06 10:12:45 +0200

    changed manifest file

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,13 +2,13 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js App',
-    short_name: 'Next.js App',
-    description: 'Next.js App',
+    name: 'Expenses Track',
+    short_name: 'Expenses Track',
+    description: 'Expenses Track',
     start_url: '/',
     display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#fff',
+    background_color: '#020817',
+    theme_color: '#020817',
     icons: [
       {
         src: '/favicon.ico',
